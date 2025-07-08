673b6368d5e284b1d73df5ff440b609e7d8aa1a8
Jose Marquez <jose.marquez012@mymdc.net>
2025-07-08 09:09:30 -0400

    feat: enhance dashboard icons with larger sizes and improved visibility

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -64,8 +64,8 @@ export default async function Dashboard() {
               <div className="p-5">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center">
-                      <span className="text-white">📊</span>
+                    <div className="w-16 h-16 bg-blue-600 flex items-center justify-center">
+                      <span className="text-white text-4xl">📊</span>
                     </div>
                   </div>
                   <div className="ml-5 w-0 flex-1">
@@ -87,8 +87,8 @@ export default async function Dashboard() {
               <div className="p-5">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <div className="w-8 h-8 bg-green-600 rounded-full flex items-center justify-center">
-                      <span className="text-white">✅</span>
+                    <div className="w-16 h-16 bg-green-600  flex items-center justify-center">
+                      <span className="text-white text-4xl">✅</span>
                     </div>
                   </div>
                   <div className="ml-5 w-0 flex-1">
@@ -110,8 +110,8 @@ export default async function Dashboard() {
               <div className="p-5">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <div className="w-8 h-8 bg-yellow-600 rounded-full flex items-center justify-center">
-                      <span className="text-white">�</span>
+                    <div className="w-16 h-16 bg-yellow-600 flex items-center justify-center">
+                      <span className="text-white text-4xl">?</span>
                     </div>
                   </div>
                   <div className="ml-5 w-0 flex-1">
@@ -133,8 +133,8 @@ export default async function Dashboard() {
               <div className="p-5">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center">
-                      <span className="text-white">📦</span>
+                    <div className="w-16 h-16 bg-blue-600 flex items-center justify-center">
+                      <span className="text-white text-4xl">📦</span>
                     </div>
                   </div>
                   <div className="ml-5 w-0 flex-1">
